9a3d21894983cf041a07bf9abca63838ebc94b99
lowdewijk <lodewijk.bogaards@gmail.com>
2024-08-18 15:22:08 +0200

    started modifying navbar

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">hello world</main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">hello world !!</main>
     </>
   );
 }
