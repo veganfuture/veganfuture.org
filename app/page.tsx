350f85d518f0f49ebe80bd80a02d762f64b2dba2
lowdewijk <lodewijk.bogaards@gmail.com>
2024-08-18 17:05:45 +0200

    Added first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
 import { Navbar } from "../components/navbar/navbar"
-
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">hello world !!</main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+        <Link href="/agenda" >Agenda</Link>
+      </main>
     </>
   );
 }
