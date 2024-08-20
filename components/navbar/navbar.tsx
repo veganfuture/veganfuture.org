d29ad988ca6e8e7bcaa9cf5a8d2dadc939ad6bc7
lowdewijk <lodewijk.bogaards@gmail.com>
2024-08-20 18:27:28 +0200

    Add logo and background color

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -76,7 +76,7 @@ export function Navbar({ className }: NavBarProps) {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Vegan Future</span>
-            <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
+            <img alt="" src="logo.png" className="h-16 w-auto" />
           </a>
         </div>
         <div className="flex lg:hidden">
